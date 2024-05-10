e35b65ee27627060be8bff8614f5c67db6f1d8d6
Dakota Smith <73180360+twofoldtech-dakota@users.noreply.github.com>
2024-05-10 09:19:18 -0500

    init

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  name: "SitecoreBits",
+  url: "https://sitecorebits.com",
+  description: "Your Ultimate Resource for Sitecore Development",
+  author: "Dakota Smith",
+  links: {
+    twitter: "https://twitter.com/daksmitty",
+    github: "https://github.com/twofoldtech-dakota",
+    linkedin: "https://www.linkedin.com/in/dakota-smith-a855b230",
+    personalSite: "https://www.twofold.tech",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
